fc00b759f2e7bcdf5d927dc56bd7adb6648a3bd6
Kestutis Baltronas <kestutisbalt@gmail.com>
2014-12-27 13:19:27 +0200

    Added dummy_data_line structure.

---
+++ src/dummy_data_line.hpp
@@ -0,0 +1,15 @@
+#ifndef DUMMY_DATA_LINE_HPP
+#define DUMMY_DATA_LINE_HPP
+
+#include <string>
+
+
+struct dummy_data_line
+{
+	std::string name;
+	std::string last_name;
+	int age;
+	std::string address;
+};
+
+#endif // DUMMY_DATA_LINE_HPP
